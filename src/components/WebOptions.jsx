866e9abfb2afb148fbd12979a19e42366fb4dd57
Aleksei Chesnokov <173167461+Aleksei-Che@users.noreply.github.com>
2024-11-03 22:04:50 +0100

    añadimos botones para WebOptions y sus estilos / Ex. 3

--- src/components/WebOptions.jsx
+++ src/components/WebOptions.jsx
@@ -1,25 +1,39 @@
 import React from "react";
 
 function WebOptions({ pages, languages, onPagesChange, onLanguagesChange }) {
+    const incrementPages = () => onPagesChange(pages + 1);
+    const decrementPages = () => onPagesChange(pages - 1);
+    const incrementLanguages = () => onLanguagesChange(languages + 1);
+    const decrementLanguages = () => onLanguagesChange(languages - 1);
+
     return (
         <div className="web-options">
             <div className="option">
                 <label>Nombre de pàgines</label>
-                <input 
+                <div className="input-group">
+                    <button onClick={decrementPages} className="btn-change">-</button>
+                    <input 
                     type="number"
                     min="1"
                     value={pages}
                     onChange={(e) => onPagesChange(Number(e.target.value))}
                  />
+                 <button onClick={incrementPages} className="btn-change">+</button>
+                </div>
             </div>
             <div className="option">
                 <label>Nombre de llenguatges</label>
-                <input 
+                <div className="input-group">
+                    <button onClick={decrementLanguages} className="btn-change">-</button>
+                    <input 
                     type="number"
                     min="1"
                     value={languages}
                     onChange={(e) => onLanguagesChange(Number(e.target.value))}
                 />
+                <button onClick={incrementLanguages} className="btn-change">+</button>
+                </div>
+                
             </div>
         </div>
     );
